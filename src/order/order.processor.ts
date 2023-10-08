1eb03c5148b34d300f93aec44e051f3957d7e590
Ebubekir Güngör <115069398+ebubekirgungor@users.noreply.github.com>
2023-10-08 23:44:15 +0300

    Update order.processor.ts

--- src/order/order.processor.ts
+++ src/order/order.processor.ts
@@ -1,10 +1,8 @@
 import { Process, Processor } from '@nestjs/bull';
-import { Logger } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
 @Processor('order-queue')
 export class OrderProcessor {
-  private readonly logger = new Logger(OrderProcessor.name);
   constructor(private readonly prisma: PrismaService) {}
   @Process('processOrder')
   async processOrder(job) {
